499b96e040018bd6220d4acf03fe2d9deedbfc09
shubh <shubhamdixit863@gmail.com>
2024-02-10 20:19:55 +0530

    simple react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header';
+import SideBar from './components/SideBar';
+import Footer from './components/Footer';
+import Banner from './components/Banner';
+const style={
+  marginTop:"100px"
+}
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <Header/>
+      <div className='row' style={style}>
+        <div className='col-3' >
+            <SideBar/>
+        </div>
+
+<div className='col-8'>
+  <Banner/>
+
+</div>
+      </div>
+
+      <Footer/>
+   
     </div>
   );
 }
---
+++ src/components/Banner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div><svg class="bd-placeholder-img bd-placeholder-img-lg img-fluid" width="100%" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Responsive image" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Responsive image</text></svg></div>
+  )
+}
+
+export default Banner
